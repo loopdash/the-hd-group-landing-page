79dd16dc9c0e9a884e4949731beaebb75b128329
Rokan <rokan.faith@gmail.com>
2024-08-29 22:52:20 +0600

    Random section on mobile
    first time load and stay then it will load randomly

--- js/script.js
+++ js/script.js
@@ -1,38 +1,25 @@
 
-// Slick Slider
-	
+// Change slider order
 
-jQuery(document).ready(function($){
+window.onload = function() {
+    const element = document.querySelector('.hero-slider');
 
-	if($(window).innerWidth() <= 900) {
-		$('.hero-slider .slide1').clone().prependTo('.hero-slider');
-		$('.hero-slider .slide2').clone().prependTo('.hero-slider');
-	}
+    if (window.innerWidth <= 900) {
+        let loadCount = localStorage.getItem('loadCount');
 
-	$('.hero-slider').slick({
-	    draggable: true,
-	    autoplay: true,
-	    autoplaySpeed: 4000,
-	    arrows: false,
-	    dots: false,
-	    fade: true,
-	    speed: 1500,
-	  	pauseOnHover:false,
-	    infinite: true,
-	    cssEase: 'linear',
-	    touchThreshold: 100,
-		rows: 2,
-		slidesToShow: 1,
-  		slidesToScroll: 1,
-		infinite: 'infiniteScroll',
-		mobileFirst: true, 
-		responsive: [
-			{
-				breakpoint: 900,
-				settings: "unslick"
-			 }
-		  ]
-	});
-
-
-});
+        if (loadCount === null) {
+            loadCount = 1;
+            localStorage.setItem('loadCount', loadCount);
+            element.style.flexDirection = 'column';
+        } 
+        else if (loadCount === '1') {
+            loadCount = 2;
+            localStorage.setItem('loadCount', loadCount);
+            element.style.flexDirection = 'column-reverse';
+        } 
+        else {
+            const random = Math.random() < 0.5 ? 'column' : 'column-reverse';
+            element.style.flexDirection = random;
+        }
+    }
+};
